fb7708569716c7215bac2543e325d95889130bdd
Vidimaster <vidimaster@yandex.com>
2023-07-16 17:58:14 +0700

    Task10 done

--- Task10/main.py
+++ Task10/main.py
@@ -1,18 +1,17 @@
-import random
+from random import randint
 
-number = int(input ())
-r = 0
+number = int(input ('Введите количество монет: \n'))
 i = 0
 side1 = 0
 side2 = 0
 res = 0
 while i < number:
-    r = random.randrange(0, 2)
-    print(r)
+    r = randint(0, 1)
+    print('\n', r)
     i += 1
     if r == 0:
         side1 += 1
     else:
         side2 += 1
 res = min(side1, side2)
-print(res)
+print(f'\nНеобходимо перевернуть всего {res} монет.')
